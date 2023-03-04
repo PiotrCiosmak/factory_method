669d80f0d0f58e6d0392b16de2dce7056551bf8a
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-03-04 18:59:35 +0100

    clientCode function added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include "creators/creator/Creator.hpp"
+
+using namespace std;
+
+void clientCode(const Creator &creator);
 
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
     return 0;
+}
+
+void clientCode(const Creator &creator)
+{
+    cout << "Client: I'm not aware of the creator's class, but it still works.\n" << creator.someOperation() << endl;
 }
