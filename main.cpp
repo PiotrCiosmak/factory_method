f8be870f63c31b561dbe39eb7393855325b54834
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-03-04 19:01:42 +0100

    main function implemented

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "creators/creator/Creator.hpp"
+#include "creators/concrete1/ConcreteCreator1.hpp"
+#include "creators/concrete2/ConcreteCreator2.hpp"
 
 using namespace std;
 
@@ -7,7 +9,18 @@ void clientCode(const Creator &creator);
 
 int main()
 {
+    cout << "App: Launched with the ConcreteCreator1.\n";
+    Creator *creator1 = new ConcreteCreator1();
+    clientCode(*creator1);
 
+    cout << endl;
+
+    cout << "App: Launched with the ConcreteCreator2.\n";
+    Creator *creator2 = new ConcreteCreator2();
+    clientCode(*creator2);
+
+    delete creator1;
+    delete creator2;
 }
 
 void clientCode(const Creator &creator)
