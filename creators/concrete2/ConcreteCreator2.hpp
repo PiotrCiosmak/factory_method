7d20fe289c24bde5f02da9b22acc706fc652df90
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-03-04 18:56:52 +0100

    The ConcreteCreator2 class added

---
+++ creators/concrete2/ConcreteCreator2.hpp
@@ -0,0 +1,18 @@
+//
+// Created by PiotrCiosmak on 04.03.2023.
+//
+
+#ifndef FACTORY_METHOD_CONCRETECREATOR2_HPP
+#define FACTORY_METHOD_CONCRETECREATOR2_HPP
+
+
+#include "../creator/Creator.hpp"
+
+class ConcreteCreator2 : public Creator
+{
+public:
+    Product *factoryMethod() const override;
+};
+
+
+#endif //FACTORY_METHOD_CONCRETECREATOR2_HPP
